5eef18f4165989a43f49c8204c96b248413ded68
JoakimHoegaas <62018628+JoakimHoegaas@users.noreply.github.com>
2021-03-17 16:45:23 +0100

    Initial commit

---
+++ ReversedCases/CaseReverser.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReversedCases
+{
+    public class CaseReverser
+    {
+        private readonly string _case;
+
+        public CaseReverser(string case1)
+        {
+            _case = case1;
+        }
+
+        public string InvertCapitalization()
+        {
+            var sb = new StringBuilder();
+            foreach(var c in _case)
+            {
+                if (Char.IsUpper(c)) sb.Append(c.ToString().ToLower());
+                else sb.Append(c.ToString().ToUpper());
+            }
+            return sb.ToString();
+        }
+    }
+}
---
+++ ReversedCases/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ReversedCases
+{
+    class Program
+    {
+        // https://edabit.com/challenge/99oN5igrbXddAjHEL
+        // SOLVED
+
+        static void Main(string[] args)
+        {
+            var case1 = new CaseReverser("Happy Birthday");
+            Console.Write(case1.InvertCapitalization());
+        }
+    }
+}
